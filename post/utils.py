a940d18eb287af5e1a548420a097fa6caee12eba
p-rayne <marlinaqua@gmail.com>
2022-06-21 08:54:12 +0300

    Feed_delete function changed

--- post/utils.py
+++ post/utils.py
@@ -6,7 +6,7 @@ def feed_create_or_add(self, request):
 
     obj, created = UserFeed.objects.get_or_create(user=user)
 
-    following_id = UserFollowing.objects.filter(user=user).values_list('following_user', flat=True)
+    following_id = user.following.values_list('following_user', flat=True)
     posts = Post.objects.filter(owner__in=following_id,
                                 date_create__gte=obj.date_update).values_list('pk', flat=True)
     obj.feed.add(*posts)
@@ -19,11 +19,10 @@ def feed_delete(self, request):
 
     obj = UserFeed.objects.get(user=user)
 
-    following_id = UserFollowing.objects.filter(user=user).exclude(following_user=following_user).values_list(
-        'following_user', flat=True)
+    following_id = user.following.exclude(following_user=following_user).values_list('following_user', flat=True)
     posts = Post.objects.filter(owner__in=following_id,
                                 date_create__gte=obj.date_update).values_list('pk', flat=True)
     obj.feed.add(*posts)
     unfollow_post = obj.feed.filter(owner_id=following_user)
-    unfollow_post.delete()
+    obj.feed.remove(*unfollow_post)
     obj.save()
